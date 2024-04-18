8647745261555dc1f816aef4607c85062eb7d021
Light Andy <andy.197197@yahoo.com>
2024-04-18 22:41:10 +0300

    ⚰️ Remove unused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,13 +22,3 @@ function copy(text) {
     console.error(error);
   }
 }
-
-window.addEventListener("scroll", () => {
-  const header = document.querySelector("header");
-
-  if (window.scrollY > 0) {
-    header.style.opacity = 0.9;
-  } else {
-    header.style.opacity = 1;
-  }
-});
